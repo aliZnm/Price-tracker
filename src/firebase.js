b64b8872791c0f7815f74214902bd9db6de499c6
Abdulrahman Ali <abdul76985@gmail.com>
2025-11-13 04:00:40 -0500

    Set up and initialized firebase.

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_jYMVwKET6JAqoATAnMj5DiXE0NA-EsY",
+  authDomain: "price-tracker-8087a.firebaseapp.com",
+  projectId: "price-tracker-8087a",
+  storageBucket: "price-tracker-8087a.firebasestorage.app",
+  messagingSenderId: "820960712692",
+  appId: "1:820960712692:web:ea5a86ff4d58df72a2cba1",
+  measurementId: "G-L5LNFW6GMR"
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
